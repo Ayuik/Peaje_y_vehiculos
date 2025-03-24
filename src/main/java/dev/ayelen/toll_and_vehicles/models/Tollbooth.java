a830d11227d682de72e3207f3968c393712f053c
Ayuik <marcosayelen@gmail.com>
2025-03-24 15:39:29 +0100

    feat: Toolbooth created with constructor. test passes

---
+++ src/main/java/dev/ayelen/toll_and_vehicles/models/Tollbooth.java
@@ -0,0 +1,34 @@
+package dev.ayelen.toll_and_vehicles.models;
+
+public class Tollbooth {
+    protected String name;
+    protected String city;
+    protected float earnings;
+
+    public Tollbooth() {
+        this.name = name;       
+        this.city = city;
+        this.earnings = 0F;
+    }
+
+    public String getName() {
+        return name;
+    }   
+
+    public void setName(String name) {
+        this.name = name;
+    }   
+
+    public String getCity() {
+        return city;
+    }   
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public float getEarnings() {
+        return earnings;
+    }
+
+}
