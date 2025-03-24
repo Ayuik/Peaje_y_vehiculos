c1c0aa1b0fbd3e508ad28b55258c99e2c0d12b73
Ayuik <marcosayelen@gmail.com>
2025-03-24 15:11:23 +0100

    test: added import, changed capital letters and inverted commas

--- src/test/java/dev/ayelen/toll_and_vehicles/models/VehicleTest.java
+++ src/test/java/dev/ayelen/toll_and_vehicles/models/VehicleTest.java
@@ -3,15 +3,18 @@ package dev.ayelen.toll_and_vehicles.models;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class VehicleTest {
 
     @Test
-    @DisplayName('Should create a vehicle with a plate and a type')
+    @DisplayName("Should create a vehicle with a plate and a type")
     void testVehicle() {
         Vehicle vehicleOne = new Vehicle();
         vehicleOne.setPlate("ABC123");
-        vehicleOne.setType(VEHICLETYPE.CAR);
+        vehicleOne.setType(VehicleType.CAR);
         assertThat(vehicleOne.getPlate()).isEqualTo("ABC123");
-        assertThat(vehicleOne.getType()).isEqualTo(VEHICLETYPE.CAR);        
+        assertThat(vehicleOne.getType()).isEqualTo(VehicleType.CAR);        
     }
 }
