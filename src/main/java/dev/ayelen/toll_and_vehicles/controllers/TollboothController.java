dc7e0a57202f305826eb8eec02037f6e931a8d31
Ayuik <marcosayelen@gmail.com>
2025-03-28 11:48:46 +0100

    feat: implemented saveVehicle. test passes

---
+++ src/main/java/dev/ayelen/toll_and_vehicles/controllers/TollboothController.java
@@ -0,0 +1,35 @@
+package dev.ayelen.toll_and_vehicles.controllers;
+
+import dev.ayelen.toll_and_vehicles.enums.VehicleKind;
+import dev.ayelen.toll_and_vehicles.models.Tollbooth;
+import dev.ayelen.toll_and_vehicles.models.Vehicle;
+
+public class TollboothController {
+
+    Tollbooth tollbooth = new Tollbooth();
+    VehicleKind kind;
+
+    public TollboothController(Tollbooth tollbooth) {
+        this.tollbooth = tollbooth;
+    }
+
+    public void saveVehicle(String plate, int type, int axles) {
+        if (type == 1) {
+            kind = VehicleKind.CAR;
+        }
+        if (type == 2) {
+            kind = VehicleKind.MOTORCYCLE;
+        }
+        ;
+        if (type == 3) {
+            kind = VehicleKind.TRUCK;
+        }
+
+        Vehicle newVehicle = new Vehicle();
+        newVehicle.setPlate(plate);
+        newVehicle.setType(kind);
+
+        tollbooth.addVehicle(newVehicle);
+    }
+
+}
