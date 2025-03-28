54ce8126c768e93c056d7b1557e9cbe766f051f8
Ayuik <marcosayelen@gmail.com>
2025-03-28 12:52:24 +0100

    feat: implemented index in views . test passes

--- src/main/java/dev/ayelen/toll_and_vehicles/views/TollboothViews.java
+++ src/main/java/dev/ayelen/toll_and_vehicles/views/TollboothViews.java
@@ -3,7 +3,10 @@ package dev.ayelen.toll_and_vehicles.views;
 public class TollboothViews {
 
     public void index() {
-        System.out.println("prueba");
+        System.out.println("======================================="); 
+        System.out.println(" Estación de Peaje: \"Peaje Central\""); 
+        System.out.println(" Ciudad: Llanera, Asturias"); 
+        
     }
 
 }
