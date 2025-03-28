8281e3815f4fbbad004010ea46981c958a494a18
Ayuik <marcosayelen@gmail.com>
2025-03-28 11:41:56 +0100

    test: saveVehicle fails

---
+++ src/test/java/dev/ayelen/toll_and_vehicles/controllers/TollboothControllerTest.java
@@ -0,0 +1,31 @@
+package dev.ayelen.toll_and_vehicles.controllers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dev.ayelen.toll_and_vehicles.models.Tollbooth;
+import dev.ayelen.toll_and_vehicles.models.Vehicle;
+
+
+public class TollboothControllerTest {
+
+    @Test
+    @DisplayName("Should create a vehicle and add it to the list with the information received from view")
+    void testSaveVehicle() {
+        int axles = 2;
+        String plate = "123asd";
+        int type = 3;
+        Tollbooth tollbooth = new Tollbooth();
+        TollboothController controller = new TollboothController(tollbooth);
+        controller.saveVehicle(plate, type, axles);
+        List list = tollbooth.getVehiclesList();
+        assertThat(list.size(), is(1));
+
+    }
+
+}
